2fda9671323aae0955ccf6728ddbf426e5c67275
Dan At Work <daniel.rigberg@gmail.com>
2016-08-04 10:19:43 -0400

    write tests for coordinate validation and collisions

--- Battleship/battleship_test.py
+++ Battleship/battleship_test.py
@@ -1,5 +1,5 @@
 import unittest
-from battleship import Battleship
+from battleship import Battleship, Ship
 
 class BattleshipTestCase(unittest.TestCase):
     def test_board_functions(self):
@@ -16,3 +16,42 @@ class BattleshipTestCase(unittest.TestCase):
         player2 = battleship.players[1]
         self.assertEqual(battleship.other_player(player1), player2)
         self.assertEqual(battleship.other_player(player2), player1)
+
+    def test_coordinate_validation(self):
+        battleship = Battleship()
+        dummy_coords = [
+            "A1",
+            "A01",
+            "A10",
+            "J1",
+            "J10",
+            "J11",
+            "W1",
+            "W11",
+            "&99",
+            "&9",
+            "A100",
+        ]
+        self.assertEqual(battleship.validate_coordinate(dummy_coords[0]), True)
+        self.assertEqual(battleship.validate_coordinate(dummy_coords[1]), True)
+        self.assertEqual(battleship.validate_coordinate(dummy_coords[2]), True)
+        self.assertEqual(battleship.validate_coordinate(dummy_coords[3]), True)
+        self.assertEqual(battleship.validate_coordinate(dummy_coords[4]), True)
+        self.assertEqual(battleship.validate_coordinate(dummy_coords[5]), False)
+        self.assertEqual(battleship.validate_coordinate(dummy_coords[6]), False)
+        self.assertEqual(battleship.validate_coordinate(dummy_coords[7]), False)
+        self.assertEqual(battleship.validate_coordinate(dummy_coords[8]), False)
+        self.assertEqual(battleship.validate_coordinate(dummy_coords[9]), False)
+        self.assertEqual(battleship.validate_coordinate(dummy_coords[10]), False)
+
+    def test_collision_during_setup(self):
+        battleship = Battleship()
+        player = battleship.players[0]
+        player.ships.append(Ship("Ship_1", ["A1", "I10"]))
+        player.ships.append(Ship("Ship_2", ["B1", "J10"]))
+
+        self.assertEqual(battleship.collision_check(player, "A1"), "Ship_1")
+        self.assertEqual(battleship.collision_check(player, "B1"), "Ship_2")
+        self.assertEqual(battleship.collision_check(player, "I10"), "Ship_1")
+        self.assertEqual(battleship.collision_check(player, "J10"), "Ship_2")
+        self.assertEqual(battleship.collision_check(player, "B6"), False)
